71f3f6eaf0c0e2f0b10c7bb511fcbf1e4186c03d
Matthew Sun <matthew@luna.local>
2015-07-28 03:27:48 -0400

    update/fix example PushNotification.js, fix odd code formatting

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -1,7 +1,6 @@
 var PushNotification = function() {
 };
 
-
 // Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
 PushNotification.prototype.register = function(successCallback, errorCallback, options) {
     if (errorCallback == null) { errorCallback = function() {}}
@@ -36,17 +35,18 @@ PushNotification.prototype.unregister = function(successCallback, errorCallback,
      cordova.exec(successCallback, errorCallback, "PushPlugin", "unregister", [options]);
 };
 
-    // Call this if you want to show toast notification on WP8
-    PushNotification.prototype.showToastNotification = function (successCallback, errorCallback, options) {
-        if (errorCallback == null) { errorCallback = function () { } }
+// Call this if you want to show toast notification on WP8
+PushNotification.prototype.showToastNotification = function (successCallback, errorCallback, options) {
+    if (errorCallback == null) { errorCallback = function () { } }
 
-        if (typeof errorCallback != "function") {
-            console.log("PushNotification.register failure: failure parameter not a function");
-            return
-        }
-
-        cordova.exec(successCallback, errorCallback, "PushPlugin", "showToastNotification", [options]);
+    if (typeof errorCallback != "function") {
+        console.log("PushNotification.register failure: failure parameter not a function");
+        return
     }
+
+    cordova.exec(successCallback, errorCallback, "PushPlugin", "showToastNotification", [options]);
+}
+
 // Call this to set the application icon badge
 PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallback, errorCallback, badge) {
     if (errorCallback == null) { errorCallback = function() {}}
@@ -66,7 +66,7 @@ PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallb
 
 //-------------------------------------------------------------------
 
-if(!window.plugins) {
+if (!window.plugins) {
     window.plugins = {};
 }
 if (!window.plugins.pushNotification) {
